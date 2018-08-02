f68f4c0b43b6bf7655c707a992d7c3efb130ce2f
Yeiconta <41449352+Yeiconta@users.noreply.github.com>
2018-08-02 18:08:09 -0500

    actualizado

--- DEMOVIEWS/DEMOVIEWS/App.xaml.cs
+++ DEMOVIEWS/DEMOVIEWS/App.xaml.cs
@@ -11,7 +11,7 @@ namespace DEMOVIEWS
 		{
 			InitializeComponent();
 
-			MainPage = new MainPage();
+			MainPage = new Page1();
 		}
 
 		protected override void OnStart ()
---
+++ DEMOVIEWS/DEMOVIEWS/Page1.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DEMOVIEWS
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page1 : ContentPage
+	{
+		public Page1 ()
+		{
+			InitializeComponent ();
+		}
+         void pickSelect(object sender,System.EventArgs e)
+        {
+            var elementoseleccionado = pickMusic.SelectedItem.ToString();
+            DisplayAlert("Genero Música", elementoseleccionado, "Aceptar");
+        }
+        void btnSimular_Clicked(object sender, System.EventArgs e)
+        {
+            var progreso = progressNum.Progress;
+            if (progreso == 1)
+            {
+                progressNum.ProgressTo(0, 250, Easing.SpringOut);
+            }
+            else
+            {
+                progressNum.ProgressTo(30, 300, Easing.Linear);
+            }
+        }
+        
+            void Handle_SearchButtonPressed(object sender, System.EventArgs e)
+            {
+                DisplayAlert("Buscando", "Buscando Resultado", "Aceptar");
+            }
+            void Handle_TextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e)
+            {
+                DisplayAlert("Cambiando", "Este texto esta cambiando", "Aceptar");
+            }
+        void Handle_ValueChanged(object sender, Xamarin.Forms.ItemVisibilityEventArgs e)
+        {
+            labCambio.Text = slider.Value.ToString();
+        }
+        void stepEvent(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            lbDisplay.Text = steeper.Value.ToString();
+
+        }
+        void Handle_Toggled(object sender, Xamarin.Forms.ToggledEventArgs e)
+        {
+            var estado = this.swtEstado.IsToggled;
+            if(this.swtEstado.IsToggled)
+            {
+                DisplayAlert(
+                    "Switch",
+                    "Estoy encendido",
+                    "Aceptar");
+            }
+            else
+            {
+                DisplayAlert(
+                    "Switch",
+                    "Estoy apagado",
+                    "Aceptar");
+
+            }
+        }
+
+
+        }
+
+    }
+
